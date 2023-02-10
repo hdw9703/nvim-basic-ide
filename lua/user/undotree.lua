574a73f93a92f3e1ffaeb47bb9f4e6513b040c30
he yong <hdw9703@gmail.com>
2023-02-10 11:41:49 +0800

    update config

--- lua/user/undotree.lua
+++ lua/user/undotree.lua
@@ -1,8 +1,9 @@
+-- 设置undotree打开聚焦点在undotree面板
 vim.cmd([[
   let g:undotree_SetFocusWhenToggle = 1
 ]])
 
-
+-- 光标定位到上次打开文件的位置
 vim.cmd([[
 if has("autocmd")
   au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
@@ -10,6 +11,7 @@ endif
 ]])
 
 
--- vim.cmd([[
---   highlight Normal guibg=NONE ctermbg=None
--- ]])
+-- 设置背景透明
+-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" } )
+-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" } )
+

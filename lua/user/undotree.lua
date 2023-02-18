1c7f213e33cdaf7d1de16e4f475b90bec78cf2c4
he yong <hdw9703@gmail.com>
2023-02-19 00:53:13 +0800

    更新了配置

--- lua/user/undotree.lua
+++ lua/user/undotree.lua
@@ -4,11 +4,11 @@ vim.cmd([[
 ]])
 
 -- 光标定位到上次打开文件的位置
-vim.cmd([[
-if has("autocmd")
-  au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
-endif
-]])
+-- vim.cmd([[
+-- if has("autocmd")
+--   au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
+-- endif
+-- ]])
 
 
 -- 设置背景透明

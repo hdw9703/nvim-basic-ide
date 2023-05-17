1838d14224ed320a739bfe3e9417cacec2a8df62
he yong <hdw9703@gmail.com>
2023-05-17 18:32:58 +0800

    启动perl debug的lsp

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -6,3 +6,8 @@ end
 require "user.lsp.mason"
 require("user.lsp.handlers").setup()
 require "user.lsp.null-ls"
+
+require'lspconfig'.perlls.setup{
+  cmd = { "perl", "-MPerl::LanguageServer", "-e", "Perl::LanguageServer::run", "--", "--port", "27011","--version", "2.5.0" }
+}
+

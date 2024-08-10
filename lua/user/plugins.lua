af6b3283a483da94e175b5407af4a4a9730c5267
he yong <hdw9703@gmail.com>
2024-08-11 05:53:04 +0800

    dap配置

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -97,9 +97,10 @@ return packer.startup(function(use)
 	use({ "lewis6991/gitsigns.nvim", commit = "f98c85e7c3d65a51f45863a34feb4849c82f240f" })
 
 	-- DAP
-	use({ "mfussenegger/nvim-dap", commit = "6b12294a57001d994022df8acbe2ef7327d30587" })
-	use({ "rcarriga/nvim-dap-ui", commit = "1cd4764221c91686dcf4d6b62d7a7b2d112e0b13" })
-	use({ "ravenxrz/DAPInstall.nvim", commit = "8798b4c36d33723e7bba6ed6e2c202f84bb300de" })
+	use({ "mfussenegger/nvim-dap" })
+	use({ "rcarriga/nvim-dap-ui" })
+	use({ "ravenxrz/DAPInstall.nvim" })
+  use "theHamsta/nvim-dap-virtual-text"
 
 	-- hdw9703 configuration
 	use({ "CRAG666/code_runner.nvim", requires = "nvim-lua/plenary.nvim" })
